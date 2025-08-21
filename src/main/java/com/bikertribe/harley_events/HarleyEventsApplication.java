5ca36a9620f2cf23b2c155d58e83078ded948606
Mariya Byehan <maria_byehan@yahoo.com>
2025-08-21 13:55:03 +0200

    Initial commit

---
+++ src/main/java/com/bikertribe/harley_events/HarleyEventsApplication.java
@@ -0,0 +1,13 @@
+package com.bikertribe.harley_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarleyEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarleyEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikertribe/harley_events/HarleyEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikertribe.harley_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarleyEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
